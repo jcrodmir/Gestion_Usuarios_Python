a8f6b0a21f783adce73e1b539c925108b087d874
jcrodmir <jcrmtrabajo@gmail.com>
2024-09-14 23:08:57 +0200

    fix: Interfaz inicial modificada

--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
 from tkinter import ttk  # Normal Tkinter.* widgets are not themed!
 from ttkthemes import ThemedTk
 import sqlite3 as sql
+import Opciones_CRUD as operaciones
 
 window = ThemedTk(theme="arc")
-
+window.geometry("400x400")
+s_button=ttk.Style()
+s_button.configure(".TButton",font=("Helvetica",20))
 
 miConexion=sql.connect("BBDD/Gestion_Usuarios")
 miCursor=miConexion.cursor()
@@ -12,11 +15,14 @@ comprobarTabla= miCursor.execute("SELECT name FROM sqlite_master WHERE name='USU
 
 
 if comprobarTabla.fetchone() is None:
-    ttk.Button(window,text="Crear").pack()
-    miCursor.execute("CREATE TABLE USUARIOS (ID INTEGER PRIMARY KEY AUTOINCREMENT,NOMBRE VARCHAR(25), APELLIDOS VARCHAR(40))")
-    
+    boton=ttk.Button(window,text="Crear",command=lambda : operaciones.interfaz(),style=".TButton").pack(padx=10,pady=10)
+    miCursor.execute("CREATE TABLE USUARIOS (ID INTEGER PRIMARY KEY AUTOINCREMENT,USERNAME VARCHAR(25), PASSWORD VARCHAR(20), NOMBRE VARCHAR(25),DIRECCION VARCHAR(50), COMENTARIO TEXT(250))")
+   
 else: 
-    ttk.Button(window,text="Conectar").pack()
+    ttk.Label(text="Gestión de Usuarios",font=("Arial",20)).place(relx=0.5,rely=0.1,anchor="n")
+    boton=ttk.Button(window,text="Conectar",command=lambda : operaciones.interfaz(window),style=".TButton").place(relx=0.5, rely=0.5, anchor="center")
+    
+    
     
 miCursor.close()
 miConexion.close()
