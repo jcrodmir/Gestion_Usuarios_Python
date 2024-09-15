d5f7f0adc3b728bdcccd348680fdf79b7a167a05
jcrodmir <jcrmtrabajo@gmail.com>
2024-09-15 16:16:56 +0200

    refactor: Main.py less lines

--- Main.py
+++ Main.py
@@ -3,27 +3,47 @@ from ttkthemes import ThemedTk
 import sqlite3 as sql
 import Opciones_CRUD as operaciones
 
-window = ThemedTk(theme="arc")
-window.geometry("400x400")
-s_button=ttk.Style()
-s_button.configure(".TButton",font=("Helvetica",20))
-
-miConexion=sql.connect("BBDD/Gestion_Usuarios")
-miCursor=miConexion.cursor()
-
-comprobarTabla= miCursor.execute("SELECT name FROM sqlite_master WHERE name='USUARIOS'")
 
 
-if comprobarTabla.fetchone() is None:
-    boton=ttk.Button(window,text="Crear",command=lambda : operaciones.interfaz(),style=".TButton").pack(padx=10,pady=10)
-    miCursor.execute("CREATE TABLE USUARIOS (ID INTEGER PRIMARY KEY AUTOINCREMENT,USERNAME VARCHAR(25), PASSWORD VARCHAR(20), NOMBRE VARCHAR(25),DIRECCION VARCHAR(50), COMENTARIO TEXT(250))")
-   
-else: 
+def creacion_Interfaz_Inicial(nombre):
+    #Principal
+    window = ThemedTk(theme="itft1")
+    window.geometry("400x400")
+    #Frame
+    frame=ttk.Frame(window,width=400,height=400)
+    frame.grid()
+    #Estilos botones
+    s_button=ttk.Style()
+    s_button.configure(".TButton",font=("Helvetica",20), padding=10)
+    #Creación Label y Button con el nombre que llegue por parametro
     ttk.Label(text="Gestión de Usuarios",font=("Arial",20)).place(relx=0.5,rely=0.1,anchor="n")
-    boton=ttk.Button(window,text="Conectar",command=lambda : operaciones.interfaz(window),style=".TButton").place(relx=0.5, rely=0.5, anchor="center")
+    ttk.Button(frame,text=nombre,command=lambda : operaciones.interfaz(window),style=".TButton").place(relx=0.5, rely=0.5, anchor="center")
+    window.mainloop()
+
+
+def inicio_programa():
+    #Crea , conecta BBDD
+    miConexion=sql.connect("BBDD/Gestion_Usuarios")
+    miCursor=miConexion.cursor()
+    #Seleccionamos dentro de squlite_master para ver si contiene la tabla usuario
+    comprobarTabla= miCursor.execute("SELECT name FROM sqlite_master WHERE name='USUARIOS'")
+
+
+    #Si la contiene se creara tabla y tendremos la interfaz con el boton crear, en caso contrario será conectar
+    if comprobarTabla.fetchone() is None:
+        creacion_Interfaz_Inicial("Crear")
+        miCursor.execute("CREATE TABLE USUARIOS (ID INTEGER PRIMARY KEY AUTOINCREMENT,USERNAME VARCHAR(25), PASSWORD VARCHAR(20), NOMBRE VARCHAR(25),DIRECCION VARCHAR(50), COMENTARIO TEXT(250))")
     
-    
-    
-miCursor.close()
-miConexion.close()
-window.mainloop()
+    else: 
+        creacion_Interfaz_Inicial("Conectar")
+        
+    #Cerramos conexion y cursor
+    miCursor.close()
+    miConexion.close()
+
+#Iniciamos Programa
+
+inicio_programa()
+
+
+
